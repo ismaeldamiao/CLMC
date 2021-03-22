2a3e3cc697ea281b3eb98ac457f74fa4ceeb7335
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-03-22 19:34:46 -0300

    Update alocar.c

--- numerical_recipes/alocar.c
+++ numerical_recipes/alocar.c
@@ -1,59 +1,30 @@
 /* *****************************************************************************
-   Funcoes para alocar memoria para as matrizes
+   Macros para alocar memoria para os vetores e matrizes
    *****************************************************************************
    E-mail: ismaellxd@gmail.com
    Site: https://ismaeldamiao.github.io/
-   *****************************************************************************
-   Copyright © 2020 Ismael Damião
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy 
-   of this software and associated documentation files (the “Software”), to 
-   deal in the Software without restriction, including without limitation the 
-   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-   sell copies of the Software, and to permit persons to whom the Software is 
-   furnished to do so, subject to the following conditions:
-
-   The above copyright notice and this permission notice shall be included in 
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-   IN THE SOFTWARE.
 ***************************************************************************** */
 #include<stdlib.h>
 
-double **dmatriz(const int lin, const int col){
-   int i;
-   double **tmp;
+/* ***
+   void *malloc(size_t)
+   Aloca memoria para uma array de tamanho size_t.
+   * A variavel array deve ser um poteiro.
+   * A variavel type deve ser o tipo de variavel (int, double...).
+   * A variavel tam deve ser a dimensao do vetor.
+*** */
+#define vetor(tam, type, array) {\
+array = (type*)malloc((size_t)((tam) * sizeof(type)));}
 
-   /* *****
-   Alocar memoria para as linhas da matriz
-   tmp recebe um array do tipo double* de tamanho lin
-   ***** */
-   tmp = (double**)malloc((size_t)(lin * sizeof(double*)));
-
-   /* *****
-   Alocar memoria para as colunas da matriz
-   a i-esima posicao de tmp recebe um array do tipo double de tamanho col
-   ***** */
-   for(i = 0; i < lin; ++i)
-      tmp[i] = (double*)malloc((size_t)(col * sizeof(double)));
-
-   return tmp;
-}
-
-double *dvetor(const int tam){
-   double *tmp;
-
-   /* *****
-   Alocar memoria para o vetor
-   tmp recebe um array do tipo double de tamanho tam
-   ***** */
-   tmp = (double*)malloc((size_t)(tam * sizeof(double)));
-
-   return tmp;
-}
+/* ***
+   Para fazer uma matriz a ideia eh primeiro fazer um vetor de ponteiros e,
+   para cada componente do vetor, fazer um vetor normal.
+   * A variavel array deve ser um poteiro de ponteiro.
+   * A variavel type deve ser o tipo de variavel (int, double...).
+   * A variavel lin deve ser a quantidade de linhas da matriz.
+   * A variavel col deve ser a quantidade de colunas da matriz.
+*** */
+#define matriz(lin, col, type, array) {\
+vetor(lin, type*, array);\
+for(int __i_index__ = 0; __i_index__ < (lin); ++__i_index__)\
+vetor(col, type, array[__i_index__]);}
